48fbfcf82f7cae35a4bd965759a6d37dd2843516
LilTzar <eeoerkeedu@gmail.com>
2022-06-13 10:37:16 -0600

    solved vowel count with or statements and pushing to an array

--- 03-vowel-count/Unsolved/vowel-count.js
+++ 03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,29 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+const vowelCount = function (str) {
+	const vowelsArr = [];
+	const allcaps = str.toUpperCase();
+	console.log(allcaps);
+
+	for (let i = 0; i < allcaps.length; i++) {
+		if (
+			allcaps[i] == "A" ||
+			allcaps[i] == "E" ||
+			allcaps[i] == "I" ||
+			allcaps[i] == "O" ||
+			allcaps[i] == "U"
+		) {
+			console.log("vowel found  ", allcaps[i]);
+			vowelsArr.push(allcaps[i]);
+			console.log("vowel added", vowelsArr);
+		} else {
+			console.log(allcaps[i]);
+		}
+	}
+
+	console.log(vowelsArr);
+	console.log(vowelsArr.length);
+	return vowelsArr.length;
+};
+
+vowelCount();
