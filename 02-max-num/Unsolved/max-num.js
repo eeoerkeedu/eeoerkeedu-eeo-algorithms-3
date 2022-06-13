f268d9e52ca32ccc9703a00177e58405f1f6019f
LilTzar <eeoerkeedu@gmail.com>
2022-06-13 09:55:02 -0600

    solved max-num in array

--- 02-max-num/Unsolved/max-num.js
+++ 02-max-num/Unsolved/max-num.js
@@ -1,3 +1,18 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+const maxNum = function (arr) {
+	let largest = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] >= largest) {
+			largest = arr[i];
+			console.log(largest);
+		} else {
+			console.log(largest);
+		}
+	}
+	console.log(largest);
+	return largest;
+};
+
+maxNum();
